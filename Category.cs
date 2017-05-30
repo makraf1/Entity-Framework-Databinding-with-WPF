e1ea47fe838424a9c0172ec8bfdf168411c61cff
raf <makhlouf.rafik@gmail.com>
2017-05-30 14:47:53 +0100

    first commit

---
+++ Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace WPFwithEFSample
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Products = new ObservableCollection<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ObservableCollection<Product> Products { get; private set; }
+    }
+}
